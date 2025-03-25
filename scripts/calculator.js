9da97b49a12496db6480b9c81fbb37ac891a6d19
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-03-25 17:19:30 +0530

    refractor: Remove unnecessary commented code

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -189,7 +189,6 @@ export class Calculator {
         this.historyList.innerHTML = "";
 
         if (!h.length) {
-            // this.historyList.innerHTML = `<span>No history available</span>`
             this.historyList.innerHTML = `<li>[ No history available ]</li>`;
             return;
         }
