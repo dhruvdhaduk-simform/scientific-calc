946ebdb0fce3234121c345906e2e1254249d09d5
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-03-25 13:26:17 +0530

    fix: Prevent storing the history which contains same query and result

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -217,7 +217,8 @@ export class Calculator {
     handleResult(e) {
         if (e.data.success) {
             this.display.set(e.data.result);
-            this.history.push(e.data.query, e.data.result);
+            if (e.data.query.toString() !== e.data.result.toString())
+                this.history.push(e.data.query, e.data.result);
         }
         else {
             alert(`Error: ${e.data.error.message}`);
